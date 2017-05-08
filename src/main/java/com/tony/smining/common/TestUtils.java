a1dc76f33ba5f2c51584f163a5519cded5f7170b
eyesonmeq <liming820829@163.com>
2017-05-08 15:11:59 +0800

    初始化

---
+++ src/main/java/com/tony/smining/common/TestUtils.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2017, All Rights Reserved. 
+ */
+package com.tony.smining.common;
+
+/**
+ * ADD FUNCTION
+ *
+ * @date: 2017年5月8日 下午3:04:05
+ * @author Tony 
+ */
+public class TestUtils {
+
+}
